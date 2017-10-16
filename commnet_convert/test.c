aeca42123b9537d291d97fef752f79631c4c93e3
root <root@localhost.localdomain>
2017-10-16 13:50:11 +0800

    first

---
+++ commnet_convert/test.c
@@ -0,0 +1,61 @@
+//abc"xy//z"
+//abcdefghigkopqrst
+
+//abc"xy/**/"
+
+/*abc"//"z*/
+
+/*a" c/*"*/
+
+//a/b
+
+/s
+
+/*a/ */
+
+/*a**/
+
+//a*b
+
+"ac/d"
+
+"*s"
+"xyz
+//int i=0;*/
+
+//*//*int i=0;
+
+// /**/int i=0;
+/* int i=0;
+ *//*
+*/
+
+/* int i=0;
+ // */int j=0;
+
+/*
+//每个区
+//每个快
+//所有
+//4096
+*/
+
+/* int i=0;*//*int j=0;
+*/
+
+/*
+ *//*
+*/int i=0;
+
+//////////////////////////////////////a
+
+//"abc~~~!!!!"
+
+char *str="hello//comment convert. ";
+
+char *str1="hello/*comment*/convert.";
+
+/*abc**/
+
+//abc"xy//z"
+//abcdefghigkopqrst
